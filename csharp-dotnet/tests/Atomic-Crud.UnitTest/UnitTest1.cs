40c2ba99978aa7bdf71c37a2834671c2fca4125c
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-07-01 19:05:17 -0300

    added project's basic structure

---
+++ csharp-dotnet/tests/Atomic-Crud.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Atomic_Crud.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
