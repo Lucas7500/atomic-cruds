939f93bffcb714c54a5989079092be9cdb81e4bd
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-06-30 22:47:37 -0300

    initial commit

---
+++ csharp-dotnet/src/Atomic-Crud.Api/Atomic-Crud.Api/Controllers/V1/PersonController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Atomic_Crud.Api.Controllers.V1
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            await Task.Delay(100);
+            return Ok(new { Message = "Its Working!!" });
+        }
+    }
+}
---
+++ csharp-dotnet/src/Atomic-Crud.Api/Atomic-Crud.Api/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = ApiVersion.Default;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
+public partial class Program() { }
