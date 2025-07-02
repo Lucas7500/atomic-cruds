db6ed73f04dd9daddc8bda5640678f3023c2d384
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-07-01 23:25:59 -0300

    refactor: extracted email regex to common class

---
+++ csharp-dotnet/src/Atomic-Crud.Domain/Utils/RegexPatterns.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Atomic_Crud.Domain.Utils
+{
+    public static partial class RegexPatterns
+    {
+        public static readonly Regex IsValidEmail = EmailRegexPattern();
+
+        [GeneratedRegex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$")]
+        private static partial Regex EmailRegexPattern();
+    }
+}
