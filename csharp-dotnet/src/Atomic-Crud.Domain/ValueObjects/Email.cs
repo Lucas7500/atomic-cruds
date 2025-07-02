d683362d2052d226c0c08852e6c7c4ec80292354
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-07-01 23:10:10 -0300

    feat: created Email Value Object class to represent emails in my app

---
+++ csharp-dotnet/src/Atomic-Crud.Domain/ValueObjects/Email.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Atomic_Crud.Domain.ValueObjects
+{
+    public sealed partial record Email
+    {
+        private const int MaxLength = 254;
+
+        public Email(string value)
+        {
+            Validate(value);
+            
+            var atIndex = value.IndexOf('@');
+
+            Value = value;
+            Local = value[..atIndex];
+            Domain = value[(atIndex + 1)..];
+        }
+
+        public string Value { get; }
+        public string Local { get; }
+        public string Domain { get; }
+
+        private static void Validate(string value)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value, nameof(value));
+
+            if (value.Length > MaxLength)
+            {
+                throw new ArgumentException($"Email cannot be longer than {MaxLength} characters.", nameof(value));
+            }
+
+            if (!EmailRegex().IsMatch(value))
+            {
+                throw new ArgumentException("Email is not in a valid format.", nameof(value));
+            }
+        }
+
+        [GeneratedRegex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$")]
+        private static partial Regex EmailRegex();
+    }
+}
